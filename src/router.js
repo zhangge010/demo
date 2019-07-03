0b2addc95808158644bd0b9541f00a29580cc737
zhangge <xtzhangge@163.com>
2019-07-03 17:08:40 +0800

    hhh

--- src/router.js
+++ src/router.js
@@ -1,25 +1,52 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Login from "./views/Login.vue";
+import User from "./views/User.vue";
+import Add from "./views/Add.vue";
 
-Vue.use(Router)
+
+Vue.use(Router);
 
 export default new Router({
   mode: 'history',
+  linkActiveClass:'active',
   base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'login',
+      component: Login
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+      path: '/add',
+      name: 'add',
+      component: Add
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: Home,
+      children:[
+        {
+          path: 'list',
+          name: 'list',
+          component:() => import(/* webpackChunkName: "list" */ './views/List.vue')
+        },
+        {
+          path: 'user',
+          name: 'user',
+          component:() => import(/* webpackChunkName: "user" */ './views/User.vue')
+        }
+      ]
+    },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    // }
   ]
 })
--- src/store.js
+++ src/store.js
@@ -1,14 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    lists:[]
   },
   mutations: {
-
+    addItem(state, value){
+      state.lists.push(value)
+    }
   },
   actions: {
 
